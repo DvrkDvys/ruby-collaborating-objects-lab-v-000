20b7df00b129dc0fd3e7af1efa8f9beb0aade478
Jordan Harris <jharris@wework.com>
2018-07-12 22:07:08 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -10,5 +10,18 @@ class Artist
   end
   
   def self.find_or_create_by_name(name)
-    if  @@all.detect {|i| i.name == song_name} == false 
-      
+    if  @@all.detect {|i| i.name == name} == false 
+      artist = Artist.new(name)
+      @@all << artist 
+      artist
+    else
+      @@all.detect {|i| i.name == name}
+    end 
+  end
+  
+  
+  
+  
+  
+  
+  
