c7c2f99517f3220cd1458d2a46d74f7c783e6c77
Jordan Harris <jharris@wework.com>
2018-07-12 21:58:58 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,13 @@
+class Artist 
+  
+  attr_accessor :name, :songs 
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+  
+  def self.find_or_create_by_name(name)
+    if  @@all.detect {|i| i.name == song_name}
